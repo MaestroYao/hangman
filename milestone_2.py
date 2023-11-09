59eec0c48716c4f7ff481a1adc0af2b6f3c2dbf7
MaestroYao <109035118+MaestroYao@users.noreply.github.com>
2023-11-09 20:57:56 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -28,3 +28,17 @@ Step 1: Using the input function, ask the user to enter a single letter.
 Step 2: Assign the input to a variable called guess.
 '''
 guess = input('Enter one single letter: ')
+
+
+'''
+Task 4
+Step 1: Check if the length of the input is equal to 1 and alphabetical.
+Step 2: If the input is valid print "Good guess!".
+Step 3: Else print "Oops! That is not a valid output"
+'''
+
+if (len(guess)== 1) and (guess.isalpha()== True):
+    print("Good guess!")
+    
+else:
+    print("Oops! That is not a valid output")
