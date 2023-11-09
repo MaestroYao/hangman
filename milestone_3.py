1c225d2c27d762a63f0174d8f6ce80706315e02f
MaestroYao <109035118+MaestroYao@users.noreply.github.com>
2023-11-09 21:49:51 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,4 @@
+import random
 '''
 Milestone 3 -- Task 1
 
@@ -18,12 +19,38 @@ Step 5:
 If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
 
 '''
+
 while True:
     guess = input("Guess a letter: ")
 
     if (len(guess)==1) and (guess.isalpha()== True):
         break
+    
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+
+'''
+-- Task 2
+Step 1:
+Create an if statement that checks if the guess is in the word.
+
+
+Step 2:
+In the body of the if statement, print "Good guess! {guess} is in the word.".
+
+
+Step 3:
+Create an else block that prints "Sorry, {guess} is not in the word. Try again."
+'''
+
+word_list = ["orange", "banana", "mango", "coconut", "pineapple"]
+word = random.choice(word_list)
+
+if guess in word:
+    print("Good guess! '" + guess+ "' is in the word.")
+else:
+    print("Sorry, '" + guess + "' is not in the word.")
+
         
 
