c7a57ce70646541cb3e7a5fc9c20c5c5297af4ca
MaestroYao <109035118+MaestroYao@users.noreply.github.com>
2023-11-09 22:04:34 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,8 @@
 import random
 '''
-Milestone 3 -- Task 1
+Milestone 3, this milestone develops some pieces of code and then it allows us to encapsulate them more organised as functions
 
-
+-- Task 1
 Step 1:
 Create a while loop and set the condition to True.
 
@@ -51,6 +51,72 @@ if guess in word:
     print("Good guess! '" + guess+ "' is in the word.")
 else:
     print("Sorry, '" + guess + "' is not in the word.")
+    
+
+'''
+Task 3
+
+Create 2 functions, check_guess and ask_for_input
+
+FOR CHECK_GUESS:
+Step 1:
+Define a function called check_guess. Pass guess as a parameter for the function.
+
+Step 2:
+Convert the guess into lower case.
+
+
+Step 3:
+Move the code that you wrote to check if the guess is in the word into this function block.
+
+
+FOR ASK_FOR_INPUT:
+
+
+Step 1:
+Define a function called ask_for_input.
+
+
+Step 2:
+Move the code that you wrote in the Iteratively check if the input is a valid guess task into this function block.
+
+
+Step 3:
+Outside the while loop, call the check_guess function to check if the guess is in the word.
+
+
+Step 4:
+Outside the function, call the ask_for_input function to test your code.
+
+'''
+
+
+word_list = ["orange", "banana", "mango", "coconut", "pineapple"]
+word = random.choice(word_list)
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print("Good guess! '" + guess+ "' is in the word.")
+    else:
+        print("Sorry, '" + guess + "' is not in the word.")
+
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if (len(guess)==1) and (guess.isalpha()== True):
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess)
+
+
+
+
+ask_for_input()
 
         
 
